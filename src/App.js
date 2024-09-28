d224fb10039a503ab0813f9a79e481f5a25f24b5
yeonee <leenayeon@sookmyung.ac.kr>
2024-09-28 20:07:24 +0900

    [Feat] 메인 화면
    1. 기본 배경화면 설정
    2. 네비게이션바, 버튼 컴포넌트
    3. 메인 스타트 화면 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import Background from './Backgroud/Background';
+import Start from './Start/Start';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div>app.js</div>
+    <Router>
+      <Background />
+      <Routes>
+        <Route path="/" element={<Start/>}/>
+      </Routes>
+    </Router>
   );
 }
 
