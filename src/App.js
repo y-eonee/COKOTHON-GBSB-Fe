0f4c42d2e9ed76cbab5cdbc650f7b1baab318d96
ukly <dlwjddnr5438@kookmin.ac.kr>
2024-09-28 20:11:41 +0900

    feat: 로그인 회원가입 프레임 제작

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './login/Login';
+import Signup from './signup/Signup';
+import Success from './test/Success';
 import './App.css';
 
 function App() {
-  return (
-    <div>app.js</div>
-  );
+    return (
+        <Router>
+            <header className="navbar">
+                <h1>Nav</h1>
+            </header>
+
+            <main>
+                <Routes>
+                    <Route path="/login" element={<Login />} />  {/* 로그인 페이지 */}
+                    <Route path="/signup" element={<Signup />} />  {/* 회원가입 페이지 */}
+                    <Route path="/success" element={<Success />} />  {/* 성공 페이지 */}
+                </Routes>
+            </main>
+        </Router>
+    );
 }
 
 export default App;
